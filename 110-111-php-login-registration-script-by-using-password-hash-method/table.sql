ac2a2a1d88370284976e572762853c529dd5e8bc
Sujeet Kumar <33665990+sujeetdiwakar@users.noreply.github.com>
2019-06-29 21:33:08 +0530

    PHP Login Registration using password_hash
    PHP Login Registration Script by using password_hash() method

---
+++ 110-111-php-login-registration-script-by-using-password-hash-method/table.sql
@@ -0,0 +1,43 @@
+--
+-- Database: `test`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(250) NOT NULL,
+  `password` varchar(250) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`) VALUES
+(1, 'amit', '5f4dcc3b5aa765d61d8327deb882cf99');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+COMMIT;
