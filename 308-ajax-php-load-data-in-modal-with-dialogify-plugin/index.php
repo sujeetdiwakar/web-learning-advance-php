9e81d032223c8af32df1543037eb3792c04da4a5
Sujeet Kumar <33665990+sujeetdiwakar@users.noreply.github.com>
2019-10-06 09:19:24 +0530

    View Dynamic Data using jQuery Dialogify
    View Dynamic Data using jQuery Dialogify with Ajax PHP

---
+++ 308-ajax-php-load-data-in-modal-with-dialogify-plugin/index.php
@@ -0,0 +1,83 @@
+
+<html>
+ <head>
+  <title>View Dynamic Data using jQuery Dialogify with Ajax PHP</title>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+  <script src="https://cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"></script>
+  <script src="https://cdn.datatables.net/1.10.12/js/dataTables.bootstrap.min.js"></script>  
+  <link rel="stylesheet" href="https://cdn.datatables.net/1.10.12/css/dataTables.bootstrap.min.css" />
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  <script src="https://www.jqueryscript.net/demo/Dialog-Modal-Dialogify/dist/dialogify.min.js"></script>
+ </head>
+ <body>
+  <div class="container">
+   <br />
+   <h3 align="center">View Dynamic Data using jQuery Dialogify with Ajax PHP</h3>
+   <br />
+   <div class="panel panel-default">
+    <div class="panel-heading">
+     <h3 class="panel-title">Employee Data</h3>
+    </div>
+    <div class="panel-body">
+     <div class="table-responsive">
+      <table id="user_data" class="table table-bordered table-striped">
+       <thead>
+        <tr>
+         <td>Name</td>
+         <td>Gender</td>
+         <td>Designation</td>
+         <td>Age</td>
+         <td>View</td>
+        </tr>
+       </thead>
+      </table>
+      
+     </div>
+    </div>
+   </div>
+  </div>
+ </body>
+</html>
+
+<script type="text/javascript" language="javascript" >
+$(document).ready(function(){
+ 
+ var dataTable = $('#user_data').DataTable({
+  "processing":true,
+  "serverSide":true,
+  "order":[],
+  "ajax":{
+   url:"fetch.php",
+   type:"POST"
+  },
+  "columnDefs":[
+   {
+    "targets":[4],
+    "orderable":false,
+   },
+  ],
+
+ });
+
+ $(document).on('click', '.view', function(){
+  var id = $(this).attr('id');
+  var options = {
+   ajaxPrefix: '',
+   ajaxData: {id:id},
+   ajaxComplete:function(){
+    this.buttons([{
+     type: Dialogify.BUTTON_PRIMARY
+    }]);
+   }
+  };
+  new Dialogify('fetch_single.php', options)
+   .title('View Employee Details')
+   .showModal();
+ });
+ 
+ 
+ 
+ 
+});
+</script>
