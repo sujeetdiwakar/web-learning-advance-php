56ee273256b6e9d8637d0a1b5e5602b397616d28
Sujeet Kumar <33665990+sujeetdiwakar@users.noreply.github.com>
2019-10-08 11:08:28 +0530

    CSV File Editing and Importing in PHP

---
+++ 317-live-csv-file-data-editing-and-importing-in-php-using-ajax-jquery/db.sql
@@ -0,0 +1,34 @@
+--
+-- Database: `testing`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_student`
+--
+
+CREATE TABLE `tbl_student` (
+  `student_id` int(11) NOT NULL,
+  `student_name` varchar(250) NOT NULL,
+  `student_phone` varchar(20) NOT NULL,
+  `image` varchar(255) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for table `tbl_student`
+--
+ALTER TABLE `tbl_student`
+  ADD PRIMARY KEY (`student_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_student`
+--
+ALTER TABLE `tbl_student`
+  MODIFY `student_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+
+
