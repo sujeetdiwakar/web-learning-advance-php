92a253848f665ceecc186f9df39bcee84f34e5ad
Sujeet Kumar <33665990+sujeetdiwakar@users.noreply.github.com>
2019-10-08 15:04:44 +0530

    How to Create Progress Bar for Data Insert in PHP
    How to Create Progress Bar for Data Insert in PHP using Ajax

---
+++ 340-how-to-create-progress-bar-for-data-insert-in-php-using-ajax/table.sql
@@ -0,0 +1,32 @@
+--
+-- Database: `testing`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_sample`
+--
+
+CREATE TABLE `tbl_sample` (
+  `id` int(11) NOT NULL,
+  `first_name` varchar(250) NOT NULL,
+  `last_name` varchar(250) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for table `tbl_sample`
+--
+ALTER TABLE `tbl_sample`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_sample`
+--
+ALTER TABLE `tbl_sample`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+
